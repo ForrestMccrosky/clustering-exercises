1938b10d91d2c182964dcf30bb40aaded0f8a08a
Your Name <forrest.mccrosky@gmail.com>
2021-06-17 16:08:30 -0500

    adding functions to wrangle_zillow

---
+++ wrangle_zillow.py
@@ -0,0 +1,99 @@
+import math
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+import seaborn as sns  
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
+
+############################## Function File ###############################
+
+############################## Summarize DF function #######################
+
+
+def summarize(df):
+    '''
+    summarize will take in a single argument (a pandas dataframe) 
+    and output to console various statistics on said dataframe, including:
+    # .head()
+    # .info()
+    # .describe()
+    # value_counts()
+    # observation of nulls in the dataframe
+    '''
+    print('=====================================================')
+    print('Dataframe head: ')
+    print(df.head(3))
+    print('=====================================================')
+    print('Dataframe info: ')
+    print(df.info())
+    print('=====================================================')
+    print('Dataframe Description: ')
+    print(df.describe())
+    
+
+########################## Value_Counts DF function #######################
+
+def df_value_counts(df):
+
+    num_cols = [col for col in df.columns if df[col].dtype != 'O']
+    cat_cols = [col for col in df.columns if col not in num_cols]
+    
+    print('DataFrame value counts: ')
+    print('=====================================================')
+    print('')
+    for col in df.columns:
+        if col in cat_cols:
+            print(df[col].value_counts())
+        else:
+            print(df[col].value_counts(bins=10, sort=False))
+            print('=====================================================')
+            print('')
+
+############################## Histogram DF function #########################
+
+def histogram_df(df):
+
+    for col in df.columns:
+        df[col].hist()
+        plt.ylabel('Frequency')
+        plt.xlabel(f'{col} value')
+        plt.title(f'Distribution of {col}')
+        plt.show()
+        
+        
+############################## Missing Values DF function #########################
+
+
+def miss_dup_values(df):
+    '''
+    this function takes a dataframe as input and will output metrics for missing values and duplicated rows,
+    and the percent of that column that has missing values and duplicated rows
+    '''
+        # Total missing values
+    mis_val = df.isnull().sum()
+        # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+        #total of duplicated
+    dup = df.duplicated().sum()
+        # Percentage of missing values
+    dup_percent = 100 * dup / len(df)
+        # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+    mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+        # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
+           " columns that have missing values.")
+    print( "  ")
+    print (f"** There are {dup} duplicate rows that represents {round(dup_percent, 2)}% of total Values**")
+        # Return the dataframe with missing information
+    return mis_val_table_ren_columns
